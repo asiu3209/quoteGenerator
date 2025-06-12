8a56b25836aaf12773608c6640d7af5a4a845c64
Aaron Siu <aarons3209@gmail.com>
2025-06-12 11:13:49 -0400

    changed from restTemplate to webservice

--- src/main/java/com/example/quoteGenerator/QuoteService.java
+++ src/main/java/com/example/quoteGenerator/QuoteService.java
@@ -4,7 +4,7 @@ import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 //quotes api
 //https://api-ninjas.com/api/quotes 
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 @Service
 public class QuoteService{
     private final String apiLink = "https://api.api-ninjas.com/v1/quotes";
@@ -12,18 +12,35 @@ public class QuoteService{
     HttpHeaders headers;
     HttpEntity<Void> entity;
 
+    // public Quote generateQuote() {
+    //     RestTemplate restTemplate = new RestTemplate();
+    //     headers = new HttpHeaders();
+    //     headers.set("X-Api-Key", apiKey);
+    //     entity = new HttpEntity<>(headers);
+    //     ResponseEntity<Quote[]> response = restTemplate.exchange(
+    //         apiLink,
+    //         HttpMethod.GET,
+    //         entity,
+    //         Quote[].class
+    //     );
+    //     Quote[] quotes = response.getBody();
+    //     return (quotes != null && quotes.length > 0) ? quotes[0] : null;
+    // }
     public Quote generateQuote() {
-        RestTemplate restTemplate = new RestTemplate();
-        headers = new HttpHeaders();
-        headers.set("X-Api-Key", apiKey);
-        entity = new HttpEntity<>(headers);
-        ResponseEntity<Quote[]> response = restTemplate.exchange(
-            apiLink,
-            HttpMethod.GET,
-            entity,
-            Quote[].class
-        );
-        Quote[] quotes = response.getBody();
+        WebClient client = WebClient.builder()
+            .baseUrl(apiLink)
+            .defaultHeader("X-Api-Key", apiKey)
+            .build();
+            //sets up the http get request
+        Quote[] quotes = client.get()
+            //executes request
+            .retrieve()
+            //converts information into a quote array
+            .bodyToMono(Quote[].class)
+            //Sync running?
+            .block();
+
         return (quotes != null && quotes.length > 0) ? quotes[0] : null;
-    }
+}
+
 }
